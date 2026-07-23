bacce166a55935d00547647fc192c75b55314784
Nidhi Sharma <ns42847@eid.utexas.edu>
2026-07-23 18:00:35 -0500

    Display Login Page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,10 +4,12 @@ import './index.css'
 import './styles/styles.css'
 import App from './App.tsx'
 import ProjectManagementPage from './pages/Project/ProjectGateway.tsx'
+import Login from './pages/Auth/Login.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    /* <App /> */
+    {/* <App /> */}
     {/* <ProjectManagementPage/>  */}
+   <Login />
   </StrictMode>,
 )
