68251d42858ff25203cd1292fe2a45aef194efa7
Rishmita Rao <rishmita.rao12@gmail.com>
2026-07-23 18:18:53 -0500

    added routes for the page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './styles/styles.css'
 import App from './App.tsx'
-import ProjectManagementPage from './pages/Project/ProjectGateway.tsx'
-import Login from './pages/Auth/Login.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
-    {/* <ProjectManagementPage/>  */}
-   <Login />
+    <App />
   </StrictMode>,
 )
