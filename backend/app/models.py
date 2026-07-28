d1c77ac8ea524a8397dadcbf0d4694c0382d9b0c
ChinmayAnandS <write2chimbu@gmail.com>
2026-07-28 13:36:02 -0500

    feat: adds pydantic models to backend

---
+++ backend/app/models.py
@@ -0,0 +1,40 @@
+# backend/app/models.py
+from pydantic import BaseModel, Field
+from typing import Optional, List
+from datetime import datetime
+
+class UserRegister(BaseModel):
+    userid: str
+    password: str
+
+class UserLogin(BaseModel):
+    userid: str
+    password: str
+
+class ProjectCreate(BaseModel):
+    name: str
+    description: str
+    project_id: str
+
+class ProjectsDashboard(BaseModel):
+    id: str = Field(alias="_id")
+    name: str
+    description: str
+    project_id: str
+    members: List[str]
+    checkouts: dict = {}   # {"HWSet1": 0, "HWSet2": 0}
+
+class JoinProject(BaseModel):
+    project_id: str
+
+class CheckoutRequest(BaseModel):
+    hwset: str            # "HWSet1" or "HWSet2"
+    quantity: int = Field(gt=0)
+
+class CheckinRequest(BaseModel):
+    hwset: str            # "HWSet1" or "HWSet2"
+    quantity: int = Field(gt=0)
+
+class TotalCheckoutForProject(BaseModel):
+    project_id: str
+    total_checkouts: dict = {}   # {"HWSet1": 0, "HWSet2": 0}
